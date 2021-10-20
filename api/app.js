e0b8f88b3f21638eeca35eb1ba4e6cc4805a2b09
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-10-20 12:17:32 +0530

    setup body-parser package

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -7,6 +8,8 @@ const PORT = 5000;
 
 // setup middlewares
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.json('api is working!');
