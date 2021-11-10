044f8ed3778af04e7fe286206bf242de390a36ad
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-11-11 01:17:34 +0530

    db reconnect

--- api/config/db.js
+++ api/config/db.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://mongo:27017/multi-tier-docker-db';
+// use when starting application as docker container, part of docker-compose
+let mongoUrlDockerCompose = 'mongodb://mongo:27017/multi-tier-docker-db';
+
+// use when starting application as k8s pod
+let mongoUrlK8s = `mongodb://${process.env.DB_URL}/multi-tier-docker-db`;
 
 const db = async () => {
-  const connection = await mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    const connection = await mongoose.connect(mongoUrlDockerCompose, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  console.log(`MongoDB connected: ${connection.connection.host}`);
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error(
+      'Failed to connect to MongoDB at startup. Retrying in 60 seconds!',
+      error
+    );
+    setTimeout(await db(), 60000);
+  }
 };
 
 module.exports = db;
